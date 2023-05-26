811373296b87c3de3d35be0e7d7639fccc383ec7
grishin <isip_p.s.grishin@mpt.ru>
2023-05-26 14:06:16 +0300

    Adding client page

--- Tkani_session/Pages/AuthPage.xaml.cs
+++ Tkani_session/Pages/AuthPage.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Tkani_session.TradeDataSetTableAdapters;
+using static System.Net.Mime.MediaTypeNames;
+using Application = System.Windows.Application;
 
 namespace Tkani_session.Pages
 {
@@ -22,12 +25,18 @@ namespace Tkani_session.Pages
     public partial class AuthPage : Page
     {
         private readonly MainWindow mainWindow;
-        UserTableAdapter userTable = new UserTableAdapter();
+        UserTableAdapter userTable;
+
+        private DispatcherTimer failedAuthTimer;
+        private TimeSpan retryAuthOffset;
 
         public AuthPage(MainWindow mainWindow)
         {
             InitializeComponent();
             this.mainWindow = mainWindow;
+            userTable = new UserTableAdapter();
+            tbLogin.Text = "1@gmail.com";
+            tbPassword.Text = "LdNyos";
         }
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
@@ -35,16 +44,49 @@ namespace Tkani_session.Pages
             string login = tbLogin.Text;
             string password = tbPassword.Text;
 
-            var user = userTable.GetData().Where(u=> u.UserLogin == login).Where(u => u.UserPassword == password).FirstOrDefault();
+            var user = userTable.GetData().Where(u => u.UserLogin == login).Where(u => u.UserPassword == password).FirstOrDefault();
 
-            if(user == null)
+            if (user == null)
             {
                 MessageBox.Show("Неправильный логин или пароль");
+                OnAuthFailed();
             }
             else
             {
-                this.NavigationService.Navigate(new CLientPage(mainWindow));
+                this.NavigationService.Navigate(new ClientPage(mainWindow, user));
             }
+        }
+
+        private void OnAuthFailed()
+        {
+            ChangeUiState(false);
+            retryAuthOffset = TimeSpan.FromSeconds(10);
+            tbTimer.Text = "Пожалуйста, подождите 10 секунд";
+            tbTimer.Visibility = Visibility.Visible;
+            failedAuthTimer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, delegate
+            {
+                OnFailedAuthTimerTick();
+            }, Application.Current.Dispatcher);
+        }
+
+        private void OnFailedAuthTimerTick()
+        {
+            var retrySecond = retryAuthOffset.Add(TimeSpan.FromSeconds(-1)).Seconds;
+            retryAuthOffset = TimeSpan.FromSeconds(retrySecond);
+            if (retrySecond <= 0)
+            {
+                ChangeUiState(true);
+                tbTimer.Visibility = Visibility.Collapsed;
+                failedAuthTimer.Stop();
+            }
+            tbTimer.Text = $"Пожалуйста, подождите {retryAuthOffset.Seconds} сек.";
+        }
+
+        private void ChangeUiState(bool state)
+        {
+            tbLogin.IsEnabled = state;
+            tbPassword.IsEnabled = state;
+            btnLogin.IsEnabled = state;
         }
     }
 }
--- Tkani_session/Pages/ClientPage.xaml.cs
+++ Tkani_session/Pages/ClientPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +15,94 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tkani_session.TradeDataSetTableAdapters;
 
 namespace Tkani_session.Pages
 {
     /// <summary>
     /// Логика взаимодействия для ClientPage.xaml
-    /// </summary>
+    /// </summary>   
     public partial class ClientPage : Page
     {
-        public ClientPage()
+        private readonly MainWindow mainWindow;
+        private readonly ProductTableAdapter productTableAdapter;
+        public ClientPage(MainWindow mainWindow, TradeDataSet.UserRow user)
         {
             InitializeComponent();
+            this.mainWindow = mainWindow;
+            productTableAdapter = new ProductTableAdapter();
+            tbUsername.Text = $"{user.UserSurname} {user.UserName}";
+            var data = productTableAdapter.GetData().ToList();
+
+            var listManufactur = data.Select(x => x.ProductManufacturer).Distinct();
+            cbManufacturer.ItemsSource = listManufactur;
+            ProductPhotoUpdating(data);
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs eArgs)
+        {
+            FilterAndSearch();
+        }
+
+        private void ProductPhotoUpdating(List<TradeDataSet.ProductRow> data)
+        {
+            tbCount.Text = $"{data.Count} из {productTableAdapter.GetData().Count}";
+            data.ForEach(e =>
+            {               
+                if (e.ProductPhoto == "")
+                {
+                    e.ProductPhoto = "picture.png";
+                }
+            });
+            data.ForEach(e => e.ProductPhoto = $"../Images/{e.ProductPhoto}");
+            dgProducts.ItemsSource = data;
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void cbManufacturer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FilterAndSearch();
+        }
+        private void FilterAndSearch()
+        {
+            var data = productTableAdapter.GetData().ToList();
+            if (tbSearch.Text != "")
+            {
+                data = data.Where(el => el.ProductName.ToLower().Contains(tbSearch.Text.ToLower())).ToList();
+            }
+
+            if (cbManufacturer.SelectedValue != null)
+            {
+                data = data.Where((el) => el.ProductManufacturer == cbManufacturer.SelectedValue.ToString()).ToList();
+            }
+
+            if (cbsort.SelectedValue != null)
+            {
+                Console.WriteLine(cbsort.SelectedValue);
+                Console.WriteLine(cbsort.SelectedIndex);
+                if (cbsort.SelectedIndex == 0)
+                {
+                    data = data.OrderBy(e => e.ProductCost).ToList();
+                }
+                if (cbsort.SelectedIndex == 1)
+                {
+                    data = data.OrderByDescending(e => e.ProductCost).ToList();
+                }
+
+                
+            }
+
+
+            ProductPhotoUpdating(data);
+        }
+
+        private void cbsort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FilterAndSearch();
         }
     }
 }
